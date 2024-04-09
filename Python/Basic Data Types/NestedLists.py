5e213254848de6ac04a42882eee8289c37235085
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-09 16:57:54 -0600

    Create NestedLists.py

---
+++ Python/Basic Data Types/NestedLists.py
@@ -0,0 +1,19 @@
+# difficulty: easy
+if __name__ == '__main__':
+    records = list()
+    scores = list()
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        
+        record = [name, score]
+        records.append(record)
+        scores.append(score)
+        
+    scores = sorted(set(scores))
+    second_lowest = scores[1]
+    output = [record[0] for record in records if record[1] == second_lowest]        
+   
+    output.sort()
+    for i in output:
+        print(i)
