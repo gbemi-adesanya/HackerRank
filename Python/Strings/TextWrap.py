21b603f178afe438ff9c53ab20fae139f5032f26
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 11:48:47 -0600

    Create TextWrap.py

---
+++ Python/Strings/TextWrap.py
@@ -0,0 +1,11 @@
+# difficulty: easy
+import textwrap
+
+def wrap(string, max_width):
+    m = textwrap.wrap(string, max_width)
+    return "\n".join(m)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
