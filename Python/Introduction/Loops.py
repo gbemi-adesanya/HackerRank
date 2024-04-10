5a1a7cf3a618c21703d14a0d1c3ad6b3f4b24032
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 12:26:40 -0600

    Update Loops.py

--- Python/Introduction/Loops.py
+++ Python/Introduction/Loops.py
@@ -1,6 +1,7 @@
 # difficulty: easy
+# problem: https://www.hackerrank.com/challenges/python-loops/problem
 if __name__ == '__main__':
     n = int(input())
     
     for i in range(n):
-        print(i**2)
+        print(i**2)  # ** == ^
