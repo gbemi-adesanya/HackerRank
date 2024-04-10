a579088b32f2a1331e1a8433ba1adaf244c3b5f1
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 16:12:12 -0600

    Update and rename StringValidatos.py to StringValidators.py

--- Python/Strings/StringValidators.py
+++ Python/Strings/StringValidators.py
@@ -1,9 +1,13 @@
-# difficulty: easy
+"""
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/string-validators/problem
+"""
+
 if __name__ == '__main__':
     s = input()
     
     alnum = alpha = digit = lower = upper = False
-    for char in s:
+    for char in s:  # If any character in the string is True for any of the categories, the category is True
         if char.isalnum():
             alnum = True
         if char.isalpha():
