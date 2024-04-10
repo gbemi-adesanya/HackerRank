63974938def007a523bfd1776fee6d048c576609
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 16:19:34 -0600

    Update Tuples.py

--- Python/Basic Data Types/Tuples.py
+++ Python/Basic Data Types/Tuples.py
@@ -1,6 +1,6 @@
 """
-difficulty: easy
-problem: https://www.hackerrank.com/challenges/python-tuples/problem
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/python-tuples/problem
 """
 
 if __name__ == '__main__':
