bf0b1d89709cc40429114fa3bc24b78feb966092
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 15:41:06 -0600

    Update MergeTheTools.py

--- Python/Strings/MergeTheTools.py
+++ Python/Strings/MergeTheTools.py
@@ -1,13 +1,15 @@
-# difficulty: medium
+"""
+Difficulty: medium
+Problem: https://www.hackerrank.com/challenges/merge-the-tools/
+"""
+
 def merge_the_tools(string, k):
     length = len(string)
-
     for i in range(0, length, k):
-        t_string = string[i : i + k]
-        # print(t_string)
+        t_string = string[i : i + k]  # Slice strings according to k
         u_string = ""
         for j in t_string:
-            if j not in u_string:
+            if j not in u_string:  # Add only unique characters
                 u_string += j
         print(u_string)
 
