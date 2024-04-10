13bef3fde65188688b41d68bf95c0ede6cfdedfb
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 12:33:44 -0600

    Update FindingThePercentage.py

--- Python/Basic Data Types/FindingThePercentage.py
+++ Python/Basic Data Types/FindingThePercentage.py
@@ -1,4 +1,8 @@
-# difficulty: easy
+"""
+difficulty: easy
+problem: https://www.hackerrank.com/challenges/finding-the-percentage/problem
+"""
+
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
