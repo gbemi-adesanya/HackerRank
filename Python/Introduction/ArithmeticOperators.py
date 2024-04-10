b70ef8dd3817cffc32482962f4da1edf3e580402
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 12:29:20 -0600

    Update ArithmeticOperators.py

--- Python/Introduction/ArithmeticOperators.py
+++ Python/Introduction/ArithmeticOperators.py
@@ -1,5 +1,7 @@
-# difficulty: easy
-# problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
+difficulty: easy
+problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
