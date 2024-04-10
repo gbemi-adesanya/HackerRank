a605633529adb42105c9bf7115b044aba8529649
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 12:35:08 -0600

    Update FindtheRunnerUpScore.py

--- Python/Basic Data Types/FindtheRunnerUpScore.py
+++ Python/Basic Data Types/FindtheRunnerUpScore.py
@@ -1,4 +1,8 @@
-# difficulty: easy
+"""
+difficulty: easy
+problem: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+"""
+
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
