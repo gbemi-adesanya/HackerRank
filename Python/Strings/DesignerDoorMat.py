e650140e05e72ef52fcdd35444097923d933e9da
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 15:30:37 -0600

    Update DesignerDoorMat.py

--- Python/Strings/DesignerDoorMat.py
+++ Python/Strings/DesignerDoorMat.py
@@ -1,22 +1,26 @@
-# difficulty: easy
+"""
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/designer-door-mat/problem
+"""
+
 if __name__ == "__main__":
-    welcome = "WELCOME"
-    w_len = len(welcome)
-    
     n, m = map(int, input().split())
     pattern = ".|."
     pattern_length = len(pattern)
-    reverse = list()
-    
-    for i in range(1, (n+1)//2):
-        pattern_no = 2*i - 1
-        length = (m - pattern_length * pattern_no)//2
-        string = "-" * length + pattern * pattern_no + "-"*length
-        reverse.append(string)
+    array = list()
+
+    for i in range(1, (n + 1) // 2):
+        pattern_no = 2 * i - 1
+        length = (m - pattern_length * pattern_no) // 2  # the number of dashes to be included with the pattern
+        string = "-" * length + pattern * pattern_no + "-" * length
+        array.append(string)
         print(string)
-    
-    w_dash_length = (m - w_len)//2
-    print("-" * w_dash_length + welcome + "-"*w_dash_length)
-    reverse.reverse()
-    for i in reverse:
+
+    word = "WELCOME"
+    word_length = len(word)
+    mid_length = (m - word_length) // 2  # the number of dashes to be included with WELCOME
+
+    print("-" * mid_length + word + "-" * mid_length)
+    array.reverse()
+    for i in array:
         print(i)
