062708f4ad0b087444046e735b62a4636454498f
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 14:57:27 -0600

    Update AlphabetRangoli.py

--- Python/Strings/AlphabetRangoli.py
+++ Python/Strings/AlphabetRangoli.py
@@ -1,56 +1,49 @@
-# difficulty: easy
+"""
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+"""
+
 def mirror(input_list):
-    center = input_list[len(input_list) - 1]
-    new = list()
-    for i in input_list:
-        # print(i)
-        if i != center:
-            # print(i)
-            new.append(i)
-        else:
-            break
+    # This function mirrors the list
+    copy = input_list.copy()
+    copy.pop()
 
-    for i in range(len((new))-1, -1, -1):
-        input_list.append(new[i])
-
+    # Append everything in copy from the back to the front
+    for i in range(len(copy)-1, -1, -1):
+        # start: len(copy) - 1; the index of the last element in copy
+        # stop: -1; the index before the first element (index 0) in copy
+        # step: -1; move backwards (negative sign) by one step (1)
+        input_list.append(copy[i])
     return input_list
     
 def print_rangoli(size):
-    dashes = 2*size - 1
     lines = list()
     for i in range(size):
         lines.append([])
 
+    # Populate arrays for mirroring
     for i in range(size-1, -1, -1):
-        letter = chr(i+97)
+        letter = chr(i+97)  # Change from unicode to character; a = 97
         for j in range(i+1):
             lines[j].append(letter)
 
     new_lines = list()
     for i in range(size):
-        if len(lines[i]) == 1:
-            new = lines[i]
-        else:
-            new = mirror(lines[i])
-        new_lines.append(new)
+        if len(lines[i]) != 1:
+            mirror(lines[i])  # Mirror arrays to complete lines
+        new_lines.append(lines[i)
 
-    new_lines.reverse()
+    new_lines.reverse()  # Array is created in the wrong order, so .reverse()
 
+    # Mirror array for printing
+    width = 2 * size - 1
     new_list = mirror(new_lines)
-    string_list = list()
     for i in range(len(new_list)):
-        c = ""
-        length = dashes - len(new_list[i])
-        c += "-" * length
-        c += "-".join(new_list[i])
-        c += "-" * length
-        string_list.append(c)
+        length = width - len(new_list[i])
         print("-" * length, end = "")
         print("-".join(new_list[i]), end="")
         print("-" * length)
-
-    string = "\n".join(string_list)
-    return string
+        
 
 if __name__ == '__main__':
     n = int(input())
