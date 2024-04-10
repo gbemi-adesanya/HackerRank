6e46ec5de3b2087dcca38edfe49c579d9b86c4a4
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 12:41:41 -0600

    Update Tuples.py

--- Python/Basic Data Types/Tuples.py
+++ Python/Basic Data Types/Tuples.py
@@ -1,4 +1,8 @@
-# difficulty: easy
+"""
+difficulty: easy
+problem: https://www.hackerrank.com/challenges/python-tuples/problem
+"""
+
 if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
