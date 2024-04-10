628c92e6a2c0fb25ddefbab797a24ce2014df4fc
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 12:40:26 -0600

    Update Lists.py

--- Python/Basic Data Types/Lists.py
+++ Python/Basic Data Types/Lists.py
@@ -1,14 +1,19 @@
-# difficulty: easy
+"""
+difficulty: easy
+problem: https://www.hackerrank.com/challenges/python-lists/problem
+"""
+
 if __name__ == '__main__':
     N = int(input())
     array = list()
     for i in range(N):
         instruction = list(input().split())
-        # print(instruction)
         command = instruction[0]
+        
         if command == "insert":
             pos, element = int(instruction[1]), int(instruction[2])
             array.insert(pos, element)
+            
         elif command == "print":
             print("[", end = "")
             for i in range(len(array)):
@@ -17,15 +22,20 @@ if __name__ == '__main__':
                 else:
                     print(array[i], end = "")
             print("]")
+            
         elif command == "remove":
             element = int(instruction[1])
             array.remove(element)
+            
         elif command == "append":
             element = int(instruction[1])
             array.append(element)
+            
         elif command == "sort":
             array.sort()
+            
         elif command == "pop":
             array.pop()
+            
         elif command == "reverse":
             array.reverse()
