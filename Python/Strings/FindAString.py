de8985a86ab64de3a47a55fb07fa235d9024ac51
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 15:36:03 -0600

    Update FindAString.py

--- Python/Strings/FindAString.py
+++ Python/Strings/FindAString.py
@@ -1,9 +1,13 @@
-# difficulty: easy
+"""
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/find-a-string/problem
+"""
+
 def count_substring(string, sub_string):
     n = len(sub_string)
     count = 0
     for i in range(len(string)):
-        if sub_string == string[i:i+n]:
+        if sub_string == string[i:i+n]:  # Slice the string according to the length of the substring
             count += 1
     return count
 
