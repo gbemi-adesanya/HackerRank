eb39f332c3420108b30526e7da6e2daea6cc54b4
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-10 16:15:17 -0600

    Update TheMinionGame.py

--- Python/Strings/TheMinionGame.py
+++ Python/Strings/TheMinionGame.py
@@ -1,11 +1,15 @@
-# difficulty: medium
+"""
+Difficulty: medium
+Problem: https://www.hackerrank.com/challenges/the-minion-game/problem
+"""
+
 def minion_game(string):
-    # your code goes here
     vowels = ["A", "E", "I", "O", "U"]
     
     kevin, stuart = 0, 0
     length = len(string)
     for i in range(length):
+        # length - i is the number of letters in the substring
         if string[i] in vowels:
             kevin += length - i
         else:
