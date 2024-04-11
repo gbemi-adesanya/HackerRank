617cf3d3a08c8c3a50cd04e77ab40a5a7b06d864
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-11 15:33:37 -0600

    Create IntegersComeInAllSizes.py

---
+++ Python/Math/IntegersComeInAllSizes.py
@@ -0,0 +1,8 @@
+"""
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+"""
+
+if __name__ == "__main__":
+    a, b, c, d = int(input()), int(input()), int(input()), int(input())
+    print(a**b + c**d)
