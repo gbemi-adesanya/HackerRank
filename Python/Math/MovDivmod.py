5cbb6c82059458d65b8490296b760737025a151b
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-04-11 15:29:43 -0600

    Create MovDivmod.py

---
+++ Python/Math/MovDivmod.py
@@ -0,0 +1,10 @@
+"""
+Difficulty: medium
+Problem: https://www.hackerrank.com/challenges/python-mod-divmod/problem
+"""
+
+if __name__ == "__main__":
+    a, b = int(input()), int(input())
+    print(a//b)
+    print(a % b)
+    print(divmod(a, b))
