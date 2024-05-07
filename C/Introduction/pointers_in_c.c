a1a0364c5ee98a221a3415fac6cd416a102e040e
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-05-07 15:46:15 -0600

    Create pointers_in_c.c

---
+++ C/Introduction/pointers_in_c.c
@@ -0,0 +1,27 @@
+/*
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/pointer-in-c/problem
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    int x, y;
+    x = *a + *b;
+    y = abs(*a - *b);
+    
+    *a = x;
+    *b = y;    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
