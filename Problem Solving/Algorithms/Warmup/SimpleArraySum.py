de23dcb9dcf0888f9937aa6aaec6a3573a6efc5e
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-05-28 14:42:55 -0600

    Create SimpleArraySum.py

---
+++ Problem Solving/Algorithms/Warmup/SimpleArraySum.py
@@ -0,0 +1,26 @@
+"""
+Language: Python
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/simple-array-sum/problem
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
