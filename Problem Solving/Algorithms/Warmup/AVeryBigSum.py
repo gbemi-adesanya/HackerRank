9b3057b61024a472426dc0d282ae73b27652aa2b
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-05-28 14:49:45 -0600

    Create AVeryBigSum.py

---
+++ Problem Solving/Algorithms/Warmup/AVeryBigSum.py
@@ -0,0 +1,27 @@
+"""
+Language: Python
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
