b1cddc6aad19ea543ea56b7ba996d117db0ec981
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-05-30 21:12:46 -0600

    Create BeautifulDaysAtTheMovies.py

---
+++ Problem Solving/Algorithms/Implementations/BeautifulDaysAtTheMovies.py
@@ -0,0 +1,35 @@
+"""
+Language: Python
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def beautifulDays(i, j, k):
+    beautiful = 0
+    for x in range(i, j+1):
+        if abs(x - int("".join(reversed(str(x))))) % k == 0:
+            beautiful += 1
+            
+    return beautiful
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+    i = int(first_multiple_input[0])
+    j = int(first_multiple_input[1])
+    k = int(first_multiple_input[2])
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
