73c0f00d6e0c9566d559094b649d2816987633d3
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-06-03 03:29:13 -0600

    Create TaumAndBday.py

---
+++ Problem Solving/Algorithms/Implementations/TaumAndBday.py
@@ -0,0 +1,37 @@
+"""
+Language: Python
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/taum-and-bday/problem
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def taumBday(b, w, bc, wc, z):
+    cost = (b * min(bc, wc+z)) + (w * min(wc, bc+z))
+    return cost
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+        b = int(first_multiple_input[0])
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+        bc = int(second_multiple_input[0])
+        wc = int(second_multiple_input[1])
+        z = int(second_multiple_input[2])
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+    fptr.close()
