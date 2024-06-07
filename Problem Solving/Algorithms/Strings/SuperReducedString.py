22151ee8e6ba129f74dcb620a8c7e66ece95304f
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-06-06 21:24:12 -0600

    Create SuperReducedString.py

---
+++ Problem Solving/Algorithms/Strings/SuperReducedString.py
@@ -0,0 +1,33 @@
+"""
+Language: Python
+Difficulty: easy
+Problem: https://www.hackerrank.com/challenges/reduced-string/problem
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def superReducedString(s):
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    return "".join(stack) if stack else "Empty String"
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+    fptr.close()
