67d10adb433cc6c9afed45d75c6d5d6d1deb980d
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-08-30 15:53:06 -0600

    Create WeatherObservationStation12.sql

---
+++ SQL/Basic Select/WeatherObservationStation12.sql
@@ -0,0 +1,6 @@
+-- Difficulty: easy
+-- Problem: https://hackerrank.com/challenges/weather-observation-station-12/problem
+
+SELECT DISTINCT city
+FROM station
+WHERE city NOT REGEXP "^[aeiou]" AND city NOT REGEXP "[aeiou]$"
