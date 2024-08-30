11469da537402f276708bb4426bc94f79d6b2d45
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2024-08-30 16:30:42 -0600

    Create The PADS.sql

---
+++ SQL/Advanced Select/The PADS.sql
@@ -0,0 +1,11 @@
+-- Difficulty: medium
+-- Problem: https://www.hackerrank.com/challenges/the-pads/problem
+
+SELECT CONCAT(name, "(", LEFT(occupation, 1), ")")
+FROM occupations
+ORDER BY name;
+
+SELECT CONCAT("There are a total of ", COUNT(occupation), " ", LOWER(occupation), "s.")
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(occupation), occupation
